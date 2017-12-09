e1f44c2bba160941ccece96bfad342cab55bfe46
Jonathan Berney <jberney@pivotal.io>
2017-12-08 21:18:49 -0800

    fix helpers paths

--- src/forms.js
+++ src/forms.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {Form, FormRow, FormCol} from 'pivotal-ui/react/forms';
 import {PrimaryButton} from 'pivotal-ui/react/buttons';
 import {Toggle} from 'pivotal-ui/react/toggle';
-import {pepperOptions} from '../../helpers/application_helper';
-import {post} from '../../helpers/fetch_helper';
+import {pepperOptions} from './helpers/application_helper';
+import {post} from './helpers/fetch_helper';
 
 export default class FormExample extends React.Component {
   render() {
